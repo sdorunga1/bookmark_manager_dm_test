daa30e6c0f54f786e99f45e1c516ad42955909f4
Sam Joseph <tansaku@gmail.com>
2015-10-01 12:49:54 +0100

    gets first feature test to pass and sets up RACK_ENV correctly

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,8 @@
-require 'sinatra'
-class BookmarkManager < Sinatra::Application
+require 'sinatra/base'
+class BookmarkManager < Sinatra::Base
 
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
 end
