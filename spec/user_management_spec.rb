554014be3cdfa8b8b0af2f49169870df0863ded0
Stefan Dorunga <stefandorunga@gmail.com>
2015-10-08 15:16:19 +0100

    Challenge 19 password confirmation

--- spec/user_management_spec.rb
+++ spec/user_management_spec.rb
@@ -6,12 +6,20 @@ feature 'User sign up' do
     expect(User.first.email).to eq('alice@example.com')
   end
 
+  scenario 'requires a matching confirmation password' do
+    # again it's questionable whether we should be testing the model at this
+    # level.  We are mixing integration tests with feature tests.
+    # However, it's convenient for our purposes.
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
+
   def sign_up(email: 'alice@example.com',
-              password: 'oranges!')
+              password: '12345678',
+              password_confirmation: '12345678')
     visit '/users/new'
-    expect(page.status_code).to eq(200)
-    fill_in :email,    with: email
+    fill_in :email, with: email
     fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
   end
 end
