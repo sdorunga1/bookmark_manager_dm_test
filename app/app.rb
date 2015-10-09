f49b38725df2e00e49fd00b7e59135cb6b7c8368
Stefan Dorunga <stefandorunga@gmail.com>
2015-10-09 16:48:07 +0100

    Refactor with partials and seperate controllers

--- app/app.rb
+++ app/app.rb
@@ -2,82 +2,12 @@ ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
 require 'sinatra/flash'
+require 'sinatra/partial'
+
 require_relative 'data_mapper_setup'
 
-class BookmarkManager < Sinatra::Base
-
-  enable :sessions
-  register Sinatra::Flash
-  use Rack::MethodOverride
-  set :session_secret, 'super secret'
-
-  get '/links' do
-    @links = Link.all
-    erb :'links/index'
-  end
-
-  post '/links' do
-    link = Link.create(url: params[:url], title: params[:title])
-    params[:tags].split.each do |tag|
-      link.tags << Tag.first_or_create(name: tag)
-    end
-    link.save
-    redirect to('/links')
-  end
-
-  get '/links/new' do
-    erb :'links/new'
-  end
-
-  get '/tags/:name' do
-    tag = Tag.first(name: params[:name])
-    @links = tag ? tag.links : []
-    erb :'links/index'
-  end
-
-  get '/users/new' do
-    @user = User.new
-    erb :'users/new'
-  end
-
-  post '/users' do
-    @user = User.new(email: params[:email],
-                       password: params[:password],
-                       password_confirmation: params[:password_confirmation])
-    # require 'byebug'; byebug
-    if @user.save
-      session[:user_id] = @user.id
-      redirect to('/links')
-    else
-      flash.now[:errors] = @user.errors.full_messages
-      erb :'users/new'
-    end
-  end
-
-  get '/sessions/new' do
-    erb :'sessions/new'
-  end
-
-  post '/sessions' do
-    user= User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
-      redirect to("/links")
-    else
-      flash.now[:errors] = ["The email or password is incorrect"]
-      erb :'sessions/new'
-    end
-  end
-
-  delete '/sessions' do
-    session[:user_id] = nil
-    flash.keep[:notice] = 'goodbye!'
-    redirect to '/links'
-  end
-
-  helpers do
-    def current_user
-      @current_user ||= User.get(session[:user_id])
-    end
-  end
-end
+require_relative 'server'
+require_relative 'controllers/links'
+require_relative 'controllers/tags'
+require_relative 'controllers/sessions'
+require_relative 'controllers/users'
